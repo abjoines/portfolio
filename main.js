362bd6922bc14fee536a74369dd90f0010159018
abjoines <joina778@newschool.edu>
2018-12-06 12:20:43 -0500

    edits

---
+++ main.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+$( document ).ready(function() {
+      $('.slider').slider();
+    });
+ $(document).ready(function(){
+      $('.parallax').parallax();
+});
